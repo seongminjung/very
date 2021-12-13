837f2f00057709741ab1abb04dd6bbed54acdfc8
sungminner <sung.minner@gmail.com>
2021-12-13 13:28:43 +0900

    Cooperates

--- src/components/Page3.js
+++ src/components/Page3.js
@@ -4,7 +4,14 @@ import "css/page3.css";
 const Page3 = () => {
   return (
     <div className="container">
-      <p className="p3-title">협력 기업</p>
+      <div className="p3-cooperates">
+        <p className="p3-cooperates__title">협력 기업</p>
+        <img
+          className="p3-cooperates__logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/368px-Google_2015_logo.svg.png"
+          alt="logo"
+        />
+      </div>
     </div>
   );
 };
