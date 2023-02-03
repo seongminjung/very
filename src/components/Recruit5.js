3ac05b1a51ce49f97f2435b5116b951b1d483842
sxj754 <sxj754@case.edu>
2023-02-03 16:31:54 -0500

    Recruit5 page text fix

--- src/components/Recruit5.js
+++ src/components/Recruit5.js
@@ -8,13 +8,12 @@ const Recruit5 = () => {
         <p className="r5-grid__title">VERY 모집 기간</p>
         <p className="r5-grid__title">VERY 지원 자격</p>
         <p className="r5-grid__description">
-          * 춘계 정기 모집 : 1학기 개강일 3주전부터 모집 예정
-          <br />* 추계 정기 모집 : 2학기 개강일 3주전부터 모집 예정
+          * 정기 모집 기간은 매 1학기와 2학기에 진행
+          <br />* 상세한 내용은 모집 기간에 각종 채널로 공고 예정
         </p>
         <p className="r5-grid__description">
           * 연세대학교 동문(학부생, 졸업생) 중 창업에 관심이 있는 자
-          <br />* 매주 화요일 오후 7시에 진행되는 정규 세션에 참가할 수 있는 자
-          <br />* 창업에 관심이 있으며 VERY의 활동에 열정으로 임할 자
+          <br />* 매주 진행되는 정규 세션에 참가할 수 있는 자
         </p>
       </div>
     </div>
